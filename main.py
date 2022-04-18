06ba05fa0d1764c1c627fa98e90dd977a5ed322e
hermajmalhi <hemrajmalhi1234@gmail.com>
2022-04-18 11:13:14 +0500

    qr code generater in pyhton advance

---
+++ main.py
@@ -0,0 +1,9 @@
+# qrcode generator in python
+import qrcode
+from PIL import Image
+qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,
+          box_size=10,border=4,)
+qr.add_data("https://github.com/hemrajmalhi")
+qr.make(fit=True)
+img=qr.make_image(fill_color="blue",back_color="white")
+img.save("hemrajmalhigithub_1.png")
